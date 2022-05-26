4169acdf6e69b937e56fe7676797073d3a1575af
jfdezd <javierfernandezdominguez@gmail.com>
2022-05-26 20:39:38 +0200

    initial commit

---
+++ build/precache-manifest.9ccc4cc80daec055cdf6db10f4145fc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "856f8595bef66fe09d1722ace1510aaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bb5280734486a7df6e8",
+    "url": "/static/css/main.512c1981.chunk.css"
+  },
+  {
+    "revision": "3ae6539dd1747f937b79",
+    "url": "/static/js/2.2eb69503.chunk.js"
+  },
+  {
+    "revision": "3bb5280734486a7df6e8",
+    "url": "/static/js/main.32f44944.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jfdezd.github.io/precache-manifest.336d894266aeaf9d3f77a9bceeb1427d.js"
+  "/precache-manifest.9ccc4cc80daec055cdf6db10f4145fc9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jfdezd.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
